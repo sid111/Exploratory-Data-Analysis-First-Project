6048cdd1a1fcfc0c9ba4c3ab7debd840e18070c0
sid111 <shaaz.sid@live.com>
2016-02-29 00:27:19 +0500

    Create Second Plot.R

---
+++ Second Plot.R
@@ -0,0 +1,13 @@
+rm(list=ls())
+#reading data into R:
+> cons<-read.table('D:/Desktop/Data Specialist/Exploratory Data Analysis/Project1/exdata_data_household_power_consumption/household_power_consumption.txt',sep=";",nrows= 2075260, header=TRUE, quote= "", strip.white=TRUE, stringsAsFactors = FALSE, na.strings= "?")
+# Subsetting the full data to obtain the data related to two days:
+sub<- subset(cons, (cons$Date == "1/2/2007" | cons$Date== "2/2/2007"))
+# Changing the class of Date variable from character to Date:
+sub$Date <- as.Date(sub$Date, format = "%d/%m/%Y")
+# Combining the Date and Time variable and creating a new column in dataset named "DateTime":
+sub$DateTime <- as.POSIXct(paste(sub$Date, sub$Time))
+# Creating the plot2:
+png("plot2.png", width = 480, height = 480)
+plot(sub$DateTime, sub$Global_active_power, type="l", ylab= "Global Active Power(kilowatts)", xlab="")
+dev.off()
